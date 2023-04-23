76424e690b0c60497d71908ca7457886323623a0
KristinaY-87 <Kristuyshaa@gmail.com>
2023-04-23 23:28:34 +0200

    fix

--- src/Search.js
+++ src/Search.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import axios from "axios";
-import FormatDate from "./FormatDate";
 import WeatherInfo from "./WeatherInfo";
 import "bootstrap/dist/css/bootstrap.css";
 
